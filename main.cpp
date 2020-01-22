1539942a4f6d4b98bed0253547a2f2ae3774a670
Geek <geek@localhost.localdomain>
2020-01-22 10:00:07 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include "iostream"
+#include "string"
+#include "box.h"
+
+using namespace std ;
+
+int main ()
+{
+
+
+   box *box_first = new box ;
+   box *box_second = new box ;
+
+
+
+//  The
+
+
+
+
+}
+
