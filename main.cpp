c8d065d17193e0b6bf8b4bbae3e7abbc20efdd61
Geek <alfredybruno@gmail.com>
2020-01-31 19:42:43 +0300

    assigned values to the first_box created

--- main.cpp
+++ main.cpp
@@ -29,6 +29,9 @@ int main ()
     //instantiton of the objects.
    box *box_first = new box ;
    box *box_second = new box ;
+   
+   //setting value for the first box
+   (*box_first).set_up(2, 3, 7) ;
 
 
 
