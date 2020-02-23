e4b8120a25cfde4a5ae24137c36c61c263ff82be
Geek <alfredybruno@gmail.com>
2020-02-23 10:13:34 +0300

    defined class on the account.cpp file

--- exceptionHandling/Account/account.cpp
+++ exceptionHandling/Account/account.cpp
@@ -3,3 +3,20 @@
 //
 
 #include "account.h"
+
+void account::cash_deposite() {
+    if (amount < 0)
+        throw 0 ;
+
+    balance += amount ;
+
+
+}
+
+void account::cash_withdraw() {
+    if (amount < 0 || amount > balance)
+        throw 0 ;
+
+    balance -= amount ;
+
+}
