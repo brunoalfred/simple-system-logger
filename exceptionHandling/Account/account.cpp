63b0af8d921140f969a5700fecdd5b5f4e579e17
Geek <alfredybruno@gmail.com>
2020-02-23 10:06:39 +0300

    first commit on the account

---
+++ exceptionHandling/Account/account.cpp
@@ -0,0 +1,5 @@
+//
+// Created by geek on 2/23/20.
+//
+
+#include "account.h"
---
+++ exceptionHandling/Account/account.h
@@ -0,0 +1,25 @@
+//
+// Created by geek on 2/23/20.
+//
+
+#ifndef ACCOUNT_ACCOUNT_H
+#define ACCOUNT_ACCOUNT_H
+
+
+class account {
+
+public:
+//    Defining public method
+    void cash_deposite () ;
+    void cash_withdraw () ;
+
+private:
+//
+    double balance  ;
+
+
+
+};
+
+
+#endif //ACCOUNT_ACCOUNT_H
