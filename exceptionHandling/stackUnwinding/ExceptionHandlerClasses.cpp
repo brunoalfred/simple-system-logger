1a88d56c51c5504a3bdeaff5e55ea11b796ccaea
bruno alfred <alfredybruno@gmail.com>
2020-10-03 21:42:22 +0300

    removed all uneccesary codes

--- exceptionHandling/stackUnwinding/ExceptionHandlerClasses.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by geek on 2/12/20.
-//
-
-#include "ExceptionHandlerClasses.h"
-#include <iostream>
-
-//implementing the methods
-
--- object oriented/oop_01/README.md
+++
@@ -1,3 +0,0 @@
-
-These are some of implementations i did while learning c++ basics (novice level)
-#include 
